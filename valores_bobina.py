dc881f44c58d364a4889277d5c770b6a80264521
Alvaro Siesquen <alvaro.siesquen@unmsm.edu.pe>
2023-10-08 02:09:50 -0500

    Haciendo más entendible el código de gráficas

--- valores_bobina.py
+++ valores_bobina.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 from bobina import *
 voltajes = [1.5, 3, 4.5, 6.5, 7.5, 25.37]
-amperaje = []
-valores_experimentales = [0.6, 1.15, 1.9, 2.14, 2.7, 8]
+valores_teoricos_corriente = []
+valores_experimentales_corriente = [0.6, 1.15, 1.9, 2.14, 2.7, 8]
 
 for i in voltajes:
-    amperaje.append(campo_magnetico(cobre, N, i, r, L)[0])
-plt.plot(voltajes,amperaje,'.-')
-plt.plot(voltajes,valores_experimentales,'.-')
+    valores_teoricos_corriente.append(campo_magnetico(cobre, N, i, r, L)[0])
+plt.plot(voltajes,valores_teoricos_corriente,'.-')
+plt.plot(voltajes,valores_experimentales_corriente,'.-')
 plt.xlabel('Voltaje (V)')
 plt.ylabel('Intensidad de corriente eléctrica (A)')
 plt.grid()
